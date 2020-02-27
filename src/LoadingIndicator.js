0e4753707432642317e5c7f220ee121a440a4fbf
Curtis Jurgensen <curtisj44@hotmail.com>
2020-02-27 10:13:36 -0800

    adding loading indicator

---
+++ src/LoadingIndicator.js
@@ -0,0 +1,18 @@
+// Dependencies
+// ------------
+
+// Libraries
+import React from 'react';
+// Styles
+import './LoadingIndicator.css';
+
+// External
+// --------
+
+// TODO: probably need some ARIA
+
+export default () => (
+  <div className="loading-indicator">
+    <div className="loading-indicator__text">Loading&hellip;</div>
+  </div>
+);
