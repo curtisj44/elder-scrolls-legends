38ec2e3c8767e7bbdd0d6799fff647f4ca3ef6b0
Curtis Jurgensen <curtisj44@hotmail.com>
2020-02-27 08:51:57 -0800

    adding baseline app with SiteHeader

---
+++ src/SiteHeader.js
@@ -0,0 +1,23 @@
+// Dependencies
+// ------------
+
+// Libraries
+import React from 'react';
+// Styles
+import './SiteHeader.css';
+
+// Internal
+// --------
+
+const SiteHeader = () => {
+  return (
+    <header className="site-header" role="banner">
+      <h1>The Elder Scrolls: Legends</h1>
+    </header>
+  );
+};
+
+// Exports
+// -------
+
+export default SiteHeader;
