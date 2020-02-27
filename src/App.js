bbefeb2423f1b82b724b5b28d41041dab53c28c1
Curtis Jurgensen <curtisj44@hotmail.com>
2020-02-27 11:17:14 -0800

    adding infinite scrolling

--- src/App.js
+++ src/App.js
@@ -2,27 +2,36 @@
 // ------------
 
 // Libraries
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 // Styles
 import './App.css';
 // Components
 import Card from './Card';
 import LoadingIndicator from './LoadingIndicator';
 import SiteHeader from './SiteHeader';
+// Utilities
+import throttle from 'lodash.throttle';
 
 // Internal
 // --------
 
-
-const useCardsEndpoint = ({ cardCount, page }) => {
+const App = () => {
   const [cards, setCards] = useState([]);
   const [isError, setIsError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [newRequest, setNewRequest] = useState(true);
+  const [page, setPage] = useState(1);
 
+  const cardsRef = useRef(null);
+
+  //
+  // Data fetching
   useEffect(() => {
-    console.log('useEffect');
+    const cardCount = 20;
 
     const fetchCards = async () => {
+      if (!newRequest) return;
+
       setIsLoading(true);
 
       // API documentation:
@@ -36,36 +45,46 @@ const useCardsEndpoint = ({ cardCount, page }) => {
       response
         .json()
         .then(data => {
-          setCards(data.cards);
+          setCards(initialCards => [...initialCards, ...data.cards]);
           setIsLoading(false);
+          setNewRequest(false);
         })
         .catch(err => {
           console.log(err);
           setIsError(true);
           setIsLoading(false);
+          setNewRequest(false);
         });
     };
 
     fetchCards();
-  }, [cardCount, page]);
+  }, [newRequest, page]);
 
-  return {
-    cards,
-    isError,
-    isLoading
-  };
-};
+  //
+  // Scroll event handling
+  useEffect(() => {
+    const handleScroll = throttle(() => {
+      if (newRequest) return;
 
-//
+      const {current: cards} = cardsRef;
+      const {innerHeight, scrollY} = window;
 
-const App = () => {
-  const [page, /*setPage*/] = useState(1);
+      const cardsBottom = cards.clientHeight + cards.offsetTop;
+      const buffer = innerHeight * .3;
 
-  const {
-    cards,
-    isError,
-    isLoading
-  } = useCardsEndpoint({ cardCount: 5, page });
+      if (innerHeight + scrollY > cardsBottom - buffer) {
+        console.log('should load more!', `initial page: ${page}`);
+        setPage(page + 1);
+        setNewRequest(true);
+      }
+    }, 250);
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [newRequest, page]);
 
   return (
     <>
@@ -77,7 +96,7 @@ const App = () => {
         { isError || (cards.length < 1 && !isLoading) ?
           <p>Uh oh. No cards were found.</p>
         :
-          <ul className="cards">
+          <ul className="cards" ref={ cardsRef }>
             { cards.map(card => <Card { ...card } key={ card.id } />) }
           </ul>
         }
